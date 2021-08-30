9197dbcce7d3ee5c34534605fca697a75a49de75
vuhoangphuc11 <vuhoangphuc11@gmail.com>
2021-08-30 16:54:32 +0700

    java freemarker

---
+++ SpringBootFreeMarker/src/main/java/com/phucvh/SpringBootFreeMakerApplication.java
@@ -0,0 +1,14 @@
+package com.phucvh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringBootFreeMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFreeMakerApplication.class, args);
+	}
+
+}
---
+++ SpringBootFreeMarker/src/main/java/com/phucvh/demo/SpringBootFreeMakerApplication.java
@@ -0,0 +1,13 @@
+package com.phucvh.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFreeMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFreeMakerApplication.class, args);
+	}
+
+}
